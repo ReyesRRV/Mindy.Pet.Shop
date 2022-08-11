e2c0ba197189b83d91cf98c156b9c9c9d91dea72
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-08-11 11:57:08 -0300

    modal mod

--- js/juguetes.js
+++ js/juguetes.js
@@ -26,32 +26,32 @@ function cardGenerator(array){
         <div class="card-info">
             <p class="text-title">${art.nombre}</p>
             <div class="w3-container">
-            <button onclick="document.getElementById('${art._id}').style.display='block'" class="w3-button w3-2021-mint mt-1 mb-2">Ver más</button>
+            
 
-            <div id="${art._id}" class="w3-modal">
+            <div id="${art.nombre}" class="w3-modal">
               <div class="w3-modal-content w3-animate-zoom w3-card-4">
                 <div class="w3-container w3-teal w3-2021-marigold">
-                  <span onclick="document.getElementById('${art._id}').style.display='none'" 
+                  <span onclick="document.getElementById('${art.nombre}').style.display='none'" 
                   class="w3-button w3-display-topright">&times;</span>
                   <h2>${art.nombre}</h2>
                 </div>
                 <div class="w3-container">
-                  <p class="w3-sans-serif ps-2">${art.descripcion}</p>
+                  <p class="w3-sans-serif ps-2 fs-3">${art.descripcion}</p>
                 </div>
                 <div class="w3-container w3-teal w3-2021-marigold">
-                  <p class="d-flex g-5">Stock: ${art.stock}</p>
+                  <p class="d-flex g-5 fs-4">Stock: ${art.stock}</p>
                 </div>
               </div>
             </div>
             </div>
+            <div class="card-footer">
+                <button onclick="document.getElementById('${art.nombre}').style.display='block'" class="w3-button w3-2021-mint mt-1 mb-2">Ver más</button>
+                <span class="text-title">$${art.precio}</span>
+                <button class="card-button">
+                    <img src="./asset/img/pngwing.com.png" style="width: 1.5rem; alt="boton comprar" id="${art._id}">
+                </button>
+            </div>
             
-        </div>
-        <div class="card-footer">
-            <span class="text-title">Stock: ${art.stock}</span>
-            <span class="text-title">$${art.precio}</span>
-            <button class="card-button" id="${art._id}">
-                <img src="./asset/img/pngwing.com.png" style="width: 1.5rem; alt="boton comprar" id="${art._id}">
-            </button>
         </div>`
         cardsCont.appendChild(card)
     });
@@ -72,4 +72,3 @@ function pushCarrito(boton) {
     }
     console.log(arrayCarrito)
 }
-//<p class="text-body">${art.descripcion}</p>
