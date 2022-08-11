d949587c2ef678346e8256a20fb564e015215b16
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-08-11 18:39:39 -0300

    tabla 2

--- js/carritoSalida.js
+++ js/carritoSalida.js
@@ -3,21 +3,29 @@ console.log(arrayCarrito);
 
 let tabla = document.getElementById("tabledatos1");
 function t(array){
+    let contador = 0
     let nombres = [...(new Set(array.map(events => events.nombre)))]
-    console.log(nombres)
+    let tabla1 = document.getElementById("tabledatos1")
     nombres.forEach(e=> {
         let cantidad = 0
-        let unit = 0
         arrayCarrito.forEach(producto =>{
             if(e == producto.nombre){
                 cantidad = ++cantidad
             }
-            unit = producto.precio
         })
-        let precioTotal = cantidad * unit
-        console.log(cantidad)
+        contador = ++contador
+        let artículo = arrayCarrito.find(p => p.nombre == e)
+        let precioTotal = cantidad * artículo.precio
         console.log(precioTotal)
+        let row = document.createElement ('tr')
+        row.innerHTML = `
+        <th scope="row">${contador}</th>
+        <td>${e}</td>
+        <td>${cantidad}</td>
+        <td>${new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(artículo.precio)}</td>
+        <td>${new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(precioTotal)}</td>
+        `
+        tabla1.appendChild(row)
     })
 }
-
 t(arrayCarrito)
